f72d475ce69cc2819d9dc5e2d2717cb3b87c41eb
Rishu Bansal <rbansal@oakbrooksolutions.com>
2020-02-25 13:27:12 -0500

    Second Commit

--- js_project_1.js
+++ js_project_1.js
@@ -1,5 +1,55 @@
+var result=[]
+switch(result.length){
+case 0:
 var rand = Math.round(Math.random()*20)
 var name=prompt("Enter the number")
-if(parseInt(name)==NaN) {console.log("Please input Number Integer")} else if(parseInt(name)==rand) 
-{console.log("You win")} else 
-{console.log("You loose")}
+if(isNaN(parseInt(name))) {alert("Please input Number Integer")} else if(parseInt(name)==rand) 
+{console.log("You win")
+ result.push("Win")
+} else 
+{console.log("You loose")
+result.push("Lost")}
+
+case 1:
+var rand = Math.round(Math.random()*20)
+var name=prompt("Enter the number")
+if(isNaN(parseInt(name))) {alert("Please input Number Integer")} else if(parseInt(name)==rand) 
+{console.log("You win")
+ result.push("Win")
+} else 
+{console.log("You loose")
+result.push("Lost")}
+
+case 2:
+var rand = Math.round(Math.random()*20)
+var name=prompt("Enter the number")
+if(isNaN(parseInt(name))) {alert("Please input Number Integer")} else if(parseInt(name)==rand) 
+{console.log("You win")
+ result.push("Win")
+} else 
+{console.log("You loose")
+result.push("Lost")}
+
+case 3:
+var rand = Math.round(Math.random()*20)
+var name=prompt("Enter the number")
+if(isNaN(parseInt(name))) {alert("Please input Number Integer")} else if(parseInt(name)==rand) 
+{console.log("You win")
+ result.push("Win")
+} else 
+{console.log("You loose")
+result.push("Lost")}
+
+case 4:
+var rand = Math.round(Math.random()*20)
+var name=prompt("Enter the number")
+if(isNaN(parseInt(name))) {alert("Please input Number Integer")} else if(parseInt(name)==rand) 
+{console.log("You win")
+ result.push("Win")
+} else 
+{console.log("You loose")
+result.push("Lost")}
+
+case 5:
+{alert("Game Over")} 
+}
